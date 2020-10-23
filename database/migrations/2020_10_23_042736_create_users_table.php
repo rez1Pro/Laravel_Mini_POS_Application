30b662d0cc56310f75f3c1706938150b48dd1e75
Developer-Rezwan <programmerrezwan@gmail.com>
2020-10-23 21:26:40 +0600

    Database Design and Creation

---
+++ database/migrations/2020_10_23_042736_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id');
+            $table->foreignId('admin_id')->nullable();
+            $table->string('name',20);
+            $table->string('email',40)->nullable();
+            $table->string('phone',15)->nullable();
+            $table->string('address',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
